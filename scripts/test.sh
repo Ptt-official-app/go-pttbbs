2a7eaac819467861181691fea1cc2ef49749f2f7
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2020-12-12 12:48:46 -0500

    remove cache/testcase/boards to be go-module

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ ! -d cache/testcase/boards ]; then
+  echo "to extract cache/testcase/boards"
+  tar -zxvf cache/testcase/boards.tar.gz -C cache/testcase
+else
+  echo "cache/testcase/boards exists"
+fi
+
 go build ./...
 gotest -v ./... -cover
 ipcrm -S 0x00007ffb
