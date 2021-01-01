e9cb07b89bca4c3aa6a016bcc62146aed225ae46
Paul Tang <cftang0827@gmail.com>
2021-01-01 13:04:49 +0800

    add LoadBoardSummary in bbs layer

---
+++ bbs/load_board_summary.go
@@ -0,0 +1,31 @@
+package bbs
+
+import (
+	"fmt"
+
+	"github.com/Ptt-official-app/go-pttbbs/ptt"
+)
+
+func LoadBoardSummary(uuserID UUserID, bboardID BBoardID) (summary *BoardSummary, err error) {
+	bid, bbb, err := bboardID.ToRaw()
+	fmt.Println(string(bbb[:]))
+	if err != nil {
+		return nil, ErrInvalidParams
+	}
+	userID, err := uuserID.ToRaw()
+	if err != nil {
+		return nil, ErrInvalidParams
+	}
+	uid, userecRaw, err := ptt.InitCurrentUser(userID)
+	if err != nil {
+		return nil, err
+	}
+
+	summaryRaw, err := ptt.LoadBoardSummary(userecRaw, uid, bid)
+	if err != nil || summaryRaw == nil {
+		return nil, err
+	}
+	summary = NewBoardSummaryFromRaw(summaryRaw)
+
+	return summary, nil
+}
