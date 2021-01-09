2a898d08727a0e49b88f0c9e853b4c92b27d3a2a
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2021-01-08 23:15:04 -0500

    run-in-docker

---
+++ scripts/run-in-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$#" != "1" ]; then
+    echo "usage: run-in-docker.sh [port]"
+    exit 255
+fi
+
+port=$1
+current_dir=`pwd`
+homename=`echo "${current_dir}"|awk -F '\/' '{print $2}'`
+if [ "${homename}" != "home" ]; then
+    echo "go-pttbbs in docker-dir should be with /home/[user]/go-pttbbs format"
+    exit 255
+fi
+
+ini_filename_prefix=`echo "${current_dir}"|awk -F '\/' '{print $3}'`
+ini_filename="${ini_filename_prefix}.ini"
+echo "ini_filename: ${ini_filename}"
+
+sed "s/^HTTP_HOST =.*/HTTP_HOST = 0.0.0.0:${port}/g" /etc/go-pttbbs/production.ini > /etc/go-pttbbs/${ini_filename}
+
+cp /srv/go-pttbbs/ptttype/00-config-production.go ptttype/00-config-production.go
+
+go build -tags production && sudo -iu bbs ${current_dir}/go-pttbbs -ini ${ini_filename}
