b2bf82701061d800a076267330ee5bd33f8bc5e7
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2021-01-09 22:07:27 -0500

    fix api.GetEmailTokenInfo

--- api/get_email_token_info_test.go
+++ api/get_email_token_info_test.go
@@ -10,7 +10,7 @@ import (
 func TestGetEmailTokenInfo(t *testing.T) {
 
 	jwt, _ := CreateEmailToken("SYSOP", "", "test@ptt.test", CONTEXT_CHANGE_EMAIL)
-	params0 := &GetEmailTokenInfoParams{Jwt: jwt}
+	params0 := &GetEmailTokenInfoParams{Jwt: jwt, Context: CONTEXT_CHANGE_EMAIL}
 	result0 := &GetEmailTokenInfoResult{UserID: "SYSOP", Email: "test@ptt.test"}
 
 	type args struct {
