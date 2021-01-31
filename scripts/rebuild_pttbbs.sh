b8bc78bc7f0f17724d4a2760ad6a90542137edf4
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2021-01-31 00:09:05 -0500

    have max-active-user as 100

--- scripts/rebuild_pttbbs.sh
+++ scripts/rebuild_pttbbs.sh
@@ -12,11 +12,14 @@ sed -i 's/cp .*//g' /tmp/build_ptt_new.sh
 # 4. bmake clean
 sed -i 's/bmake all/bmake clean all/g' /tmp/build_ptt_new.sh
 
-# 4. add NOKILLWATERBALL
+# 5. add NOKILLWATERBALL
 echo "#define NOKILLWATERBALL" >> /home/bbs/pttbbs/pttbbs.conf
 
-# 5. add HOTBOARDCACHE
-echo "#define HOTBOARDCACHE 10" >> /home/bbs/pttbbs/pttbbs.conf
+# 6. add HOTBOARDCACHE
+echo "#define HOTBOARDCACHE (10)" >> /home/bbs/pttbbs/pttbbs.conf
 
-# 6. rebuild
+# 7. update max-active user to 100
+sed -i 's/.*#define MAX_ACTIVE.*/#define MAX_ACTIVE (100)/g' /home/bbs/pttbbs/pttbbs.conf
+
+# 7. rebuild
 bash /tmp/build_ptt_new.sh
