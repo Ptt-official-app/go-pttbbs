a09295ed4f2adb2d0ca7e3ec3981dc7eab7526cd
Paul Tang <cftang0827@gmail.com>
2021-03-02 23:14:56 +0900

    support git version tag in run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 ini_filename=02-config.run.ini
+package=github.com/Ptt-official-app/go-pttbbs/types
+commit=$(shell git rev-parse --short HEAD)
 
-go build -tags dev && ./go-pttbbs -ini ${ini_filename}
+go build -ldflags "-X ${package}.GIT_VERSION=${commit}" -tags dev && ./go-pttbbs -ini ${ini_filename}
