b4c7a7201b2230d6b25f123acf1bb3bf007f24f1
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2021-04-18 23:03:02 -0400

    update max-user as 200k in docker

--- scripts/docker_initbbs.sh
+++ scripts/docker_initbbs.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 if [ "$#" != "2" ]; then
-    echo "usage: docker_initbbs.sh [BBSHOME] [GOBBS_IMAGE]"
+    echo "usage: docker_initbbs.sh [BBSHOME] [GOPTTBBS_IMAGE]"
     exit 255
 fi
 
 BBSHOME=$1
-GOBBS_IMAGE=$2
-echo "BBSHOME: ${BBSHOME} GOBBS_IMAGE: ${GOBBS_IMAGE}"
+GOPTTBBS_IMAGE=$2
+echo "BBSHOME: ${BBSHOME} GOPTTBBS_IMAGE: ${GOPTTBBS_IMAGE}"
 
-project=go-pttbbs
-
-docker container stop ${project}
-docker container rm ${project}
-docker run --name ${project} -v ${BBSHOME}:/home/bbs ${GOBBS_IMAGE} /opt/bbs/bin/initbbs -DoIt
+project=go-pttbbs-initbbs
 
 docker container stop ${project}
 docker container rm ${project}
-docker run --name ${project} -v ${BBSHOME}:/home/bbs ${GOBBS_IMAGE} sh -c 'mkdir -p /home/bbs/bin && cp /opt/bbs/bin/* /home/bbs/bin'
+docker run --name ${project} -v ${BBSHOME}:/home/bbs ${GOPTTBBS_IMAGE} /opt/bbs/bin/initbbs -DoIt
 
 docker container stop ${project}
 docker container rm ${project}
-docker run --name ${project} -v ${BBSHOME}:/home/bbs ${GOBBS_IMAGE} sh -c 'mkdir -p /home/bbs/etc && cp /opt/bbs/etc/* /home/bbs/etc'
+docker run --name ${project} -v ${BBSHOME}:/home/bbs ${GOPTTBBS_IMAGE} sh -c 'mkdir -p /home/bbs/bin && cp /opt/bbs/bin/* /home/bbs/bin'
 
 docker container stop ${project}
 docker container rm ${project}
-docker run --name ${project} -v ${BBSHOME}:/home/bbs ${GOBBS_IMAGE} sh -c 'mkdir -p /home/bbs/wsproxy && cp -R /opt/bbs/wsproxy/* /home/bbs/wsproxy'
+docker run --name ${project} -v ${BBSHOME}:/home/bbs ${GOPTTBBS_IMAGE} sh -c 'mkdir -p /home/bbs/etc && cp /opt/bbs/etc/* /home/bbs/etc'
+
+docker container stop ${project}
+docker container rm ${project}
+docker run --name ${project} -v ${BBSHOME}:/home/bbs ${GOPTTBBS_IMAGE} sh -c 'mkdir -p /home/bbs/wsproxy && cp -R /opt/bbs/wsproxy/* /home/bbs/wsproxy'
