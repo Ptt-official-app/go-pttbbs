c48f5367d0b9b0314c9cc535470541a494aebc46
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2022-03-11 11:32:23 -0500

    mand

--- scripts/rebuild_pttbbs.sh
+++ scripts/rebuild_pttbbs.sh
@@ -18,7 +18,7 @@ sed -i 's/pmake all/pmake clean all/g' /tmp/build_ptt_new.sh
 sed -i '/^pmake install/a ## boardd\ncd ${BBSHOME}/pttbbs/daemon/boardd\npmake clean all\ncp ${BBSHOME}/pttbbs/daemon/boardd/boardd ${BBSHOME}/bin' /tmp/build_ptt_new.sh
 
 # 4.2. mand
-sed -i '/^pmake install/a ## mand\ncd ${BBSHOME}/pttbbs/daemon/mand\npmake clean all\ncp ${BBSHOME}/pttbbs/daemon/mand/mandd ${BBSHOME}/bin' /tmp/build_ptt_new.sh
+sed -i '/^pmake install/a ## mand\ncd ${BBSHOME}/pttbbs/daemon/mand\npmake clean all\ncp ${BBSHOME}/pttbbs/daemon/mand/mand ${BBSHOME}/bin' /tmp/build_ptt_new.sh
 
 # 5. add NOKILLWATERBALL
 echo "#define NOKILLWATERBALL" >> /home/bbs/pttbbs/pttbbs.conf
