4a7922702283dfe3fe633c19cac7610920b41be5
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2025-01-19 11:36:45 -0500

    update scripts/run.sh

--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # build with tag=dev and run go-pttbbs with 02-config.run.ini.
 
+tags=dev
 ini_filename=docs/config/02-config.dev.ini
-package=github.com/Ptt-official-app/go-pttbbs/types
+types_package=github.com/Ptt-official-app/go-pttbbs/types
 commit=`git rev-parse --short HEAD`
 version=`git describe --tags`
 
 echo "commit: ${commit} version: ${version}"
 
 cd go-pttbbs
-go build -ldflags "-X ${package}.GIT_VERSION=${commit} -X ${package}.VERSION=${version}" -tags dev 
+go build -ldflags "-X ${types_package}.GIT_VERSION=${commit} -X ${types_package}.VERSION=${version}" -tags ${tags}
 cd ..
 
 ./go-pttbbs/go-pttbbs -ini ${ini_filename}
--- scripts/run-production.sh
+++ scripts/run-production.sh
@@ -9,8 +9,12 @@ fi
 
 tags=production
 ini_filename=$1
+types_package=github.com/Ptt-official-app/go-pttbbs/types
+commit=`git rev-parse --short HEAD`
+version=`git describe --tags`
 
-echo "to build: tags: ${tags} ini: %{ini_filename}"
-go build -tags ${tags}
+echo "to build: tags: ${tags} ini: %{ini_filename} commit: ${commit} version: ${version}"
+
+go build -ldflags "-X ${types_package}.GIT_VERSION=${commit} -X ${types_package}.VERSION=${version}" -tags ${tags}
 echo "to run go-pttbbs"
 go-pttbbs -ini ${ini_filename}
